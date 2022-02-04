98cd977233daf7a3ecee132be2cefefcd61b62fc
tohidi <s.tohidi22@gmail.com>
2022-02-04 15:49:47 +0330

    init

---
+++ setup.py
@@ -0,0 +1,51 @@
+import setuptools
+
+CLASSIFIERS = [
+    "Environment :: Web Environment",
+    "Intended Audience :: Developers",
+    "License :: OSI Approved :: BSD License",
+    "Operating System :: OS Independent",
+    "Programming Language :: Python",
+    "Programming Language :: Python :: 3.6",
+    "Programming Language :: Python :: 3.7",
+    "Programming Language :: Python :: 3.8",
+    "Topic :: Internet :: WWW/HTTP",
+    "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
+    "Topic :: Software Development :: Libraries :: Application Frameworks",
+    "Topic :: Software Development :: Libraries :: Python Modules",
+]
+
+
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+
+setuptools.setup(
+    name="payment-package-YOUR-USERNAME-HERE",
+    version="0.0.1",
+    author="Bahman Shadmehr & Saeide Tohidi",
+    author_email="...",
+    description="A Django Packeges for Payment",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/pypa/PaymentPackeges",
+    project_urls={
+        "Bug Tracker": "https://github.com/....",
+    },
+    classifiers=CLASSIFIERS,
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    install_requires=[
+        "Django>=2.2,<4",
+        "prices>=1.0.0",
+        "django-prices>=2.2.0",
+        "stripe>=2.63.0",
+        "requests>=2.26.0",
+        "enmerkar>=0.7.1",
+    ],
+    platforms=["any"],
+    zip_safe=False,
+    include_package_data=True,
+
+)
